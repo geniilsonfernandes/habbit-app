76241cadd241f9e861ff5ff8406b66a30f2f21a6
genilson <geniilsonfernandes@gmail.com>
2023-04-07 11:42:00 -0300

    feat(styles):add  var styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -17,7 +17,7 @@ const GlobalStyles = createGlobalStyle`
   }
 
   body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+   font-family: 'Rubik', sans-serif;
   }
 `
 
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -22,12 +22,31 @@ export default {
       200: '#454545',
     },
   },
-  fontSizes: {
-    md: '1.6rem',
-    sm: '1.2rem',
+  spacings: {
+    xxsmall: '0.8rem',
+    xsmall: '1.6rem',
+    small: '2.4rem',
+    medium: '3.2rem',
+    large: '4.0rem',
+    xlarge: '4.8rem',
+    xxlarge: '5.6rem',
   },
   radius: {
     md: '8px',
     sm: '4px',
+  },
+  font: {
+    family: "'Rubik', sans-serif",
+    normal: 500,
+    bold: 600,
+    sizes: {
+      xsmall: '1.2rem',
+      small: '1.4rem',
+      medium: '1.6rem',
+      large: '1.8rem',
+      xlarge: '2.0rem',
+      xxlarge: '2.8rem',
+      huge: '5.2rem',
+    },
   },
 } as const
