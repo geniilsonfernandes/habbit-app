4fd1a2e41943c2d9cfe5f0773968f9d5372dedbe
genilson <geniilsonfernandes@gmail.com>
2023-04-07 00:47:02 -0300

    feat(habit check): create tests

--- src/components/CheckHabit/test.tsx
+++ src/components/CheckHabit/test.tsx
@@ -1,14 +1,51 @@
-import { render, screen } from '@testing-library/react'
-import { renderWithTheme } from 'utils/renderWithTheme'
+import { act, screen } from '@testing-library/react'
 import CheckHabit from '.'
+import theme from '../../styles/theme'
+import renderWithTheme from '../../utils/renderWithTheme'
+
+const props = {
+  day: 'day',
+  date: 2,
+}
 
 describe('<CheckHabit />', () => {
-  it('should render the heading', () => {
-    renderWithTheme(
-    <CheckHabit />)
+  it('should show day and date when have prop', () => {
+    renderWithTheme(<CheckHabit {...props} />)
 
-    expect(
-      screen.getByRole('heading', { name: /CheckHabit/i })
-    ).toBeInTheDocument()
+    expect(screen.getByText(props.day)).toHaveTextContent('day')
+    expect(screen.getByText(props.date)).toBeInTheDocument()
+  })
+
+  it('should have background color green', () => {
+    renderWithTheme(<CheckHabit {...props} status="success" />)
+
+    const wrapperDate = screen.getByText(props.date)
+
+    expect(wrapperDate).toHaveStyle({
+      backgroundColor: theme.colors.habit.green[600],
+    })
+  })
+  it('should call function when clicked', () => {
+    const onClick = jest.fn()
+
+    renderWithTheme(
+      <CheckHabit {...props} onClick={onClick} status="success" />,
+    )
+
+    const wrapperDate = screen.getByText(props.date)
+
+    expect(wrapperDate).toHaveStyle({
+      backgroundColor: theme.colors.habit.green[600],
+    })
+
+    act(() => {
+      wrapperDate.click()
+    })
+
+    expect(wrapperDate).toHaveStyle({
+      backgroundColor: theme.colors.habit.orange[600],
+    })
+
+    expect(onClick).toBeCalledWith('waiting')
   })
 })
