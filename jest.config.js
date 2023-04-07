45c92e80e4f8e384aaa61dad8b2e2e1d0ba35a35
genilson <geniilsonfernandes@gmail.com>
2023-04-07 11:41:36 -0300

    jest: configure resolve path

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
+  moduleNameMapper: {
+    '^components/(.*)$': '<rootDir>/src/components/$1',
+  },
   collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
 }
