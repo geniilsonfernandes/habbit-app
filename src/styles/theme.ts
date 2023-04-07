b533f629b1a53eb29ff1a5e86362e4918481a109
genilson <geniilsonfernandes@gmail.com>
2023-04-07 02:51:40 -0300

    feat: add new styles

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,5 +1,15 @@
 export default {
   colors: {
+    text: {
+      white: {
+        100: '#FCFCFD',
+        300: '#A9A9AA',
+      },
+      black: {
+        100: '#181818',
+        300: '#454545',
+      },
+    },
     habit: {
       red: { 600: '#662020', 500: '#DD2626' },
       green: { 600: '#22442C', 500: '#43AB65' },
@@ -9,6 +19,15 @@ export default {
       500: '#181818',
       400: '#222222',
       300: '#303030',
+      200: '#454545',
     },
+  },
+  fontSizes: {
+    md: '1.6rem',
+    sm: '1.2rem',
+  },
+  radius: {
+    md: '8px',
+    sm: '4px',
   },
 } as const
