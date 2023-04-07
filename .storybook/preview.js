534f302660d878db30a4d9e8333e130368fc26a2
genilson <geniilsonfernandes@gmail.com>
2023-04-07 10:52:11 -0300

    storybook: add dark mode as dafault

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,21 @@
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from '../src/styles/global'
 import theme from '../src/styles/theme'
+export const parameters = {
+  backgrounds: {
+    default: 'habit-dark',
+    values: [
+      {
+        name: 'habit-light',
+        value: '#fff',
+      },
+      {
+        name: 'habit-dark',
+        value: '#181818',
+      },
+    ],
+  },
+}
 export const decorators = [
   (Story) => (
     <ThemeProvider theme={theme}>
