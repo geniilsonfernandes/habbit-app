c7fbbc996d3df73cff08cdd32bd354fcafe69754
genilson <geniilsonfernandes@gmail.com>
2023-04-08 23:09:12 -0300

    jest: configure resolve module paths

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,13 @@ module.exports = {
   collectCoverage: true,
   moduleNameMapper: {
     '^components/(.*)$': '<rootDir>/src/components/$1',
+    '^shared/(.*)$': '<rootDir>/src/shared/$1',
   },
   collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  modulePaths: [
+    '<rootDir>/src',
+    '<rootDir>/src/shared/habit/helper',
+    // ...
+  ],
 }
