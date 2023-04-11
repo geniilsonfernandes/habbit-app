31265989f04bce90337b3172cec65e532f0ea81d
genilson <geniilsonfernandes@gmail.com>
2023-04-10 23:49:23 -0300

    feat(jest config): add midiamatch mock

--- .jest/setup.ts
+++ .jest/setup.ts
@@ -1,2 +1,13 @@
 import '@testing-library/jest-dom'
 import 'jest-styled-components'
+
+// Mock matchMedia
+window.matchMedia =
+  window.matchMedia ||
+  function () {
+    return {
+      matches: false,
+      addListener: function () {},
+      removeListener: function () {},
+    }
+  }
