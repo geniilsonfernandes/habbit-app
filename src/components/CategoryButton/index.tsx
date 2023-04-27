ff82da5de67102c216478b6de4608842f409b85b
Genilson fernandes <geniilsonfernandes@gmail.com>
2023-04-27 08:16:52 -0300

    feat: adjuste category

--- src/components/CategoryButton/index.tsx
+++ src/components/CategoryButton/index.tsx
@@ -2,23 +2,23 @@ import * as S from './styles'
 
 export type CategoryButtonProps = {
   icon?: React.ReactNode
-  title?: string
-  color?: string
   onClick?: () => void
-}
+} & React.ButtonHTMLAttributes<HTMLButtonElement> &
+  S.WrapperProps
 
 const CategoryButton = ({
   color = 'trasparent',
   icon,
   onClick,
   title,
+  ...props
 }: CategoryButtonProps) => {
   const handleClick = () => {
     onClick && onClick()
   }
 
   return (
-    <S.Wrapper onClick={handleClick} color={color} title={title}>
+    <S.Wrapper onClick={handleClick} color={color} title={title} {...props}>
       {icon && <S.Icon>{icon}</S.Icon>}
     </S.Wrapper>
   )
--- src/components/CategoryButton/styles.ts
+++ src/components/CategoryButton/styles.ts
@@ -1,13 +1,54 @@
 import styled, { css } from 'styled-components'
 
-type IColorProps = {
+export type WrapperProps = {
   color: string
+  isActive: boolean
 }
 
-export const Wrapper = styled.button<IColorProps>`
-  ${({ color }) => css`
+export const Wrapper = styled.button<WrapperProps>`
+  ${({ color, theme, isActive }) => css`
     background-color: ${color};
+    width: 48px;
+    height: 48px;
+    border-radius: 16px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    cursor: pointer;
+
+    border: none;
+    outline: none;
+
+    transition: ${theme.transitions.bounce};
+
+    &:disabled {
+      opacity: 0.5;
+    }
+
+    &:hover {
+      filter: brightness(0.9);
+    }
+
+    ${isActive &&
+    css`
+      box-shadow: 0px 0px 0px 4px ${theme.colors.primary[100]};
+
+      filter: brightness(0.9);
+      &:hover {
+        filter: brightness(1);
+      }
+
+      &:disabled {
+        opacity: 1;
+      }
+    `}
   `}
 `
 
-export const Icon = styled.div``
+export const Icon = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
