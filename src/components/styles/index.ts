7b6e2159cc590f8e1ab1713e627b975352991807
Genilson fernandes <geniilsonfernandes@gmail.com>
2023-04-27 08:19:15 -0300

    feat: create grid and flex helper componente

--- src/components/styles/index.ts
+++ src/components/styles/index.ts
@@ -1,4 +1,5 @@
 import styled, { css } from 'styled-components'
+import theme from 'styles/theme'
 
 const Card = styled.div`
   ${({ theme }) => css`
@@ -8,4 +9,39 @@ const Card = styled.div`
   `}
 `
 
-export { Card }
+type GridProps = {
+  columns?: number
+  gap?: keyof typeof theme.spacings
+}
+
+const Grid = styled.div<GridProps>`
+  ${({ theme, columns = 1, gap = 'large' }) => css`
+    display: grid;
+    grid-template-columns: repeat(${columns}, 1fr);
+    gap: ${theme.spacings[gap]};
+  `}
+`
+
+type FlexProps = {
+  direction?: 'row' | 'column'
+  justify?: 'center' | 'flex-start' | 'flex-end'
+  align?: 'center' | 'flex-start' | 'flex-end'
+  gap?: keyof typeof theme.spacings
+}
+const Flex = styled.div<FlexProps>`
+  ${({
+    theme,
+    direction = 'row',
+    justify = 'flex-start',
+    align = 'flex-start',
+    gap = 'large',
+  }) => css`
+    display: flex;
+    flex-direction: ${direction};
+    justify-content: ${justify};
+    align-items: ${align};
+    gap: ${theme.spacings[gap]};
+  `}
+`
+
+export { Card, Grid, Flex }
